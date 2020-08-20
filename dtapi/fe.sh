17dc01bd13d449865251eb970a4de0ca5dbabe2b
Roman Mamchur <r_mamchur@ukr.net>
2020-08-20 22:29:54 +0300

    fix bug

--- dtapi/fe.sh
+++ dtapi/fe.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-echo 'pwd' $(pwd) >/vagrant/lll
-echo 'id' $(id) >>/vagrant/lll
-echo 'home' $HOME >>/vagrant/lll
-
 #yum update -y
 yum install -y mc
-#yum install yum-utils -y
+yum install yum-utils -y
 yum install -y wget
-#yum install -y unzip
-#yum install -y git
-#yum install -y nano
+yum install -y unzip
+yum install -y git
+yum install -y nano
 yum install httpd -y
 systemctl enable httpd
 
@@ -29,6 +25,14 @@ EOF
 
 chown apache:apache /var/www/ -R
 chmod 777 -R /var/www/
+
+mkdir /root/.ssh
+cp /home/vagrant/.ssh/authorized_keys /root/.ssh
+cp /vagrant/insecure_private_key  /root/.ssh/id_rsa
+chown root:root /root/.ssh/id_rsa  /root/.ssh/authorized_keys
+chmod 600 /root/.ssh/id_rsa /root/.ssh/authorized_keys
+
+systemctl restart sshd
 
 chcon -t httpd_sys_rw_content_t /var/www/dtapi -R
 setsebool -P httpd_can_network_connect on
